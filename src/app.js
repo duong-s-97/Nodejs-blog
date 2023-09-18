875e499fac700359436841ec3aa73b2ca38c121e
duong-s-97 <duongdev97@gmail.com>
2023-09-18 16:17:05 +0700

    set up folder

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+
 const app = express();
 const port = 3000;
 
+// HTTP Logger
 app.use(morgan("combined"));
 
 app.get("/", (req, res) => {
