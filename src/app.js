5d03b3af4e2a119d74f51736fd1904e14554b78e
duong-s-97 <duongdev97@gmail.com>
2023-09-19 11:37:09 +0700

    add sass + boostrap

--- src/app.js
+++ src/app.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const morgan = require("morgan");
+const { engine } = require("express-handlebars");
+const path = require("path");
 
 const app = express();
 const port = 3000;
 
+// CONFLICS FILES TINH~
+app.use(express.static(path.join(__dirname, "public")));
+
 // HTTP Logger
 app.use(morgan("combined"));
 
+// Template engine
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", "./src/resources/views");
+
 app.get("/", (req, res) => {
-  res.send("Hello BaBeeee");
+  res.render("home");
 });
 
 app.listen(port, () => {
